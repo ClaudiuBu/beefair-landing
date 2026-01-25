b6c9776b3405a17f4521c7195bfda3e25231738d
Daniel Butnaru <danielbutnaru44@gmail.com>
2026-01-26 00:01:45 +0200

    push de test

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ return (
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-col">
         <h1 className="text-4xl font-bold">BeeFair 🐝</h1>
         <p className="text-xl">Nu te mai certa pe bani.</p>
+        <p>push facut de daniel</p>
         
         {/* Aici pui formularul de Waitlist */}
         <button className="bg-yellow-500 text-black px-4 py-2 rounded mt-4">
