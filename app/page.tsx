a1a0d3346ad784240ee15058997b1f51069f9ccb
Claudiu Butnaru <cbutnaru@seniorsoftware.ro>
2026-01-25 23:58:52 +0200

    center flex

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
 return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-yellow-400">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-yellow-400 items-center">
       {/* Aici vine Hero Section */}
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-col">
         <h1 className="text-4xl font-bold">BeeFair 🐝</h1>
         <p className="text-xl">Nu te mai certa pe bani.</p>
         
